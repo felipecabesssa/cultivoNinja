6b298acad9fc59c6c4b9dcbb0727b08f8d8685ec
felipecabesssa <felipesoares100@hotmail.com>
2026-05-29 02:15:45 -0300

    Configura pra acessar no celular (só local)

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -25,7 +25,7 @@ const salvarRega = async () => {
   };
   
   try {
-    const response = await fetch('http://localhost:3000/api/rega', {
+    const response = await fetch('http://192.168.0.7/api/rega', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload)
